3fb3bb3f1ae632e348adf6f8aad14ade7bdf4b27
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-04-12 10:33:13 +0700

    Update set_defaults.sh

--- debian/set_defaults.sh
+++ debian/set_defaults.sh
@@ -2,20 +2,20 @@
 
 DEFAULTS_FILE="/etc/sing-box/defaults.conf"
 
-# 提示用户输入参数，如果为空则使用默认值
-read -rp "请输入后端地址: " BACKEND_URL
+# Prompt the user to enter a parameter, and use the default value if it is empty
+read -rp "Please enter the backend address: " BACKEND_URL
 BACKEND_URL=${BACKEND_URL:-$(grep BACKEND_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-read -rp "请输入订阅地址: " SUBSCRIPTION_URL
+read -rp "Please enter the subscription address: " SUBSCRIPTION_URL
 SUBSCRIPTION_URL=${SUBSCRIPTION_URL:-$(grep SUBSCRIPTION_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-read -rp "请输入TProxy配置文件地址: " TPROXY_TEMPLATE_URL
+read -rp "Please enter the TProxy configuration file address: " TPROXY_TEMPLATE_URL
 TPROXY_TEMPLATE_URL=${TPROXY_TEMPLATE_URL:-$(grep TPROXY_TEMPLATE_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-read -rp "请输入TUN配置文件地址: " TUN_TEMPLATE_URL
+read -rp "Please enter the TUN configuration file address: " TUN_TEMPLATE_URL
 TUN_TEMPLATE_URL=${TUN_TEMPLATE_URL:-$(grep TUN_TEMPLATE_URL $DEFAULTS_FILE | cut -d '=' -f2)}
 
-# 更新默认配置文件
+# Update the default configuration file
 cat > $DEFAULTS_FILE <<EOF
 BACKEND_URL=$BACKEND_URL
 SUBSCRIPTION_URL=$SUBSCRIPTION_URL
@@ -23,4 +23,4 @@ TPROXY_TEMPLATE_URL=$TPROXY_TEMPLATE_URL
 TUN_TEMPLATE_URL=$TUN_TEMPLATE_URL
 EOF
 
-echo "默认配置已更新"
+echo "Default configuration updated"
