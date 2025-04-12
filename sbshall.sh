ca32cd61e1130789bc9b06d492b7133781d558eb
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-04-12 09:06:30 +0700

    Update sbshall.sh

--- sbshall.sh
+++ sbshall.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
-# 定义主脚本的下载URL
-DEBIAN_MAIN_SCRIPT_URL="https://ghfast.top/https://raw.githubusercontent.com/qichiyuhub/sbshell/refs/heads/master/debian/menu.sh"
-OPENWRT_MAIN_SCRIPT_URL="https://ghfast.top/https://raw.githubusercontent.com/qichiyuhub/sbshell/refs/heads/master/openwrt/menu.sh"
+# Defines the download URL for the main script
+DEBIAN_MAIN_SCRIPT_URL="https://ghfast.top/https://raw.githubusercontent.com/Yogxx/sbshell/refs/heads/master/debian/menu.sh"
+OPENWRT_MAIN_SCRIPT_URL="https://ghfast.top/https://raw.githubusercontent.com/Yogxx/sbshell/refs/heads/master/openwrt/menu.sh"
 
-# 脚本下载目录
+# Script download directory
 SCRIPT_DIR="/etc/sing-box/scripts"
 
-# 定义颜色
+# Defining Colors
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
-NC='\033[0m' # 无颜色
+NC='\033[0m' # No Color
 
-# 检查系统是否支持
+# Check whether the system supports
 if [[ "$(uname -s)" != "Linux" ]]; then
-    echo -e "${RED}当前系统不支持运行此脚本。${NC}"
+    echo -e "${RED}The current system does not support running this script.${NC}"
     exit 1
 fi
 
-# 检查发行版并下载相应的主脚本
+# Check the release and download the corresponding master script
 if grep -qi 'debian\|ubuntu\|armbian' /etc/os-release; then
-    echo -e "${GREEN}系统为Debian/Ubuntu/Armbian,支持运行此脚本。${NC}"
+    echo -e "${GREEN}The system is Debian/Ubuntu/Armbian, which supports running this script.${NC}"
     MAIN_SCRIPT_URL="$DEBIAN_MAIN_SCRIPT_URL"
     DEPENDENCIES=("wget" "nftables")
 
-    # 检查 sudo 是否安装
+    # Check if sudo is installed
     if ! command -v sudo &> /dev/null; then
-        echo -e "${RED}sudo 未安装。${NC}"
-        read -rp "是否安装 sudo?(y/n): " install_sudo
+        echo -e "${RED}sudo is not installed.${NC}"
+        read -rp "Do you want to install sudo? (y/n): " install_sudo
         if [[ "$install_sudo" =~ ^[Yy]$ ]]; then
             apt-get update
             apt-get install -y sudo
             if ! command -v sudo &> /dev/null; then
-                echo -e "${RED}安装 sudo 失败，请手动安装 sudo 并重新运行此脚本。${NC}"
+                echo -e "${RED}Installation of sudo failed, please install sudo manually and rerun this script.${NC}"
                 exit 1
             fi
-            echo -e "${GREEN}sudo 安装成功。${NC}"
+            echo -e "${GREEN}sudo was installed successfully.${NC}"
         else
-            echo -e "${RED}由于未安装 sudo,脚本无法继续运行。${NC}"
+            echo -e "${RED}Since sudo is not installed, the script cannot proceed.${NC}"
             exit 1
         fi
     fi
 
-    # 检查并安装缺失的依赖项
+    # Check and install missing dependencies
     for DEP in "${DEPENDENCIES[@]}"; do
         if [ "$DEP" == "nftables" ]; then
             CHECK_CMD="nft --version"
@@ -52,27 +52,27 @@ if grep -qi 'debian\|ubuntu\|armbian' /etc/os-release; then
 
         if ! $CHECK_CMD &> /dev/null; then
             echo -e "${RED}$DEP 未安装。${NC}"
-            read -rp "是否安装 $DEP?(y/n): " install_dep
+            read -rp "Do you want to install $DEP? (y/n): " install_dep
             if [[ "$install_dep" =~ ^[Yy]$ ]]; then
                 sudo apt-get update
                 sudo apt-get install -y "$DEP"
                 if ! $CHECK_CMD &> /dev/null; then
-                    echo -e "${RED}安装 $DEP 失败，请手动安装 $DEP 并重新运行此脚本。${NC}"
+                    echo -e "${RED}Installation of $DEP failed, please install $DEP manually and rerun this script.${NC}"
                     exit 1
                 fi
-                echo -e "${GREEN}$DEP 安装成功。${NC}"
+                echo -e "${GREEN}$DEP Installation was successful.${NC}"
             else
-                echo -e "${RED}由于未安装 $DEP,脚本无法继续运行。${NC}"
+                echo -e "${RED}The script cannot continue because $DEP is not installed.${NC}"
                 exit 1
             fi
         fi
     done
 elif grep -qi 'openwrt' /etc/os-release; then
-    echo -e "${GREEN}系统为OpenWRT,支持运行此脚本。${NC}"
+    echo -e "${GREEN}The system is OpenWRT, which supports running this script.${NC}"
     MAIN_SCRIPT_URL="$OPENWRT_MAIN_SCRIPT_URL"
     DEPENDENCIES=("nftables")
 
-    # 检查并安装缺失的依赖项
+    # Check and install missing dependencies
     for DEP in "${DEPENDENCIES[@]}"; do
         if [ "$DEP" == "nftables" ]; then
             CHECK_CMD="nft --version"
@@ -80,27 +80,27 @@ elif grep -qi 'openwrt' /etc/os-release; then
 
         if ! $CHECK_CMD &> /dev/null; then
             echo -e "${RED}$DEP 未安装。${NC}"
-            read -rp "是否安装 $DEP?(y/n): " install_dep
+            read -rp "Do you want to install $DEP? (y/n): " install_dep
             if [[ "$install_dep" =~ ^[Yy]$ ]]; then
                 opkg update
                 opkg install "$DEP"
                 if ! $CHECK_CMD &> /dev/null; then
-                    echo -e "${RED}安装 $DEP 失败，请手动安装 $DEP 并重新运行此脚本。${NC}"
+                    echo -e "${RED}Installation of $DEP failed, please install $DEP manually and rerun this script.${NC}"
                     exit 1
                 fi
-                echo -e "${GREEN}$DEP 安装成功。${NC}"
+                echo -e "${GREEN}$DEP Installation was successful.${NC}"
             else
-                echo -e "${RED}由于未安装 $DEP,脚本无法继续运行。${NC}"
+                echo -e "${RED}The script cannot continue because $DEP is not installed.${NC}"
                 exit 1
             fi
         fi
     done
 else
-    echo -e "${RED}当前系统不是Debian/Ubuntu/Armbian/OpenWRT,不支持运行此脚本。${NC}"
+    echo -e "${RED}The current system is not Debian/Ubuntu/Armbian/OpenWRT and does not support running this script.${NC}"
     exit 1
 fi
 
-# 确保脚本目录存在并设置权限
+# Make sure the script directory exists and set permissions
 if grep -qi 'openwrt' /etc/os-release; then
     mkdir -p "$SCRIPT_DIR"
 else
@@ -108,18 +108,18 @@ else
     sudo chown "$(whoami)":"$(whoami)" "$SCRIPT_DIR"
 fi
 
-# 下载并执行主脚本
+# Download and execute the main script
 if grep -qi 'openwrt' /etc/os-release; then
     curl -s -o "$SCRIPT_DIR/menu.sh" "$MAIN_SCRIPT_URL"
 else
     wget -q -O "$SCRIPT_DIR/menu.sh" "$MAIN_SCRIPT_URL"
 fi
 
-echo -e "${GREEN}脚本下载中,请耐心等待...${NC}"
-echo -e "${YELLOW}注意:安装更新singbox尽量使用代理环境,运行singbox切记关闭代理!${NC}"
+echo -e "${GREEN}The script is downloading, please wait patiently...${NC}"
+echo -e "${YELLOW}Note: Try to use a proxy environment when installing and updating singbox, and remember to turn off the proxy when running singbox!${NC}"
 
 if ! [ -f "$SCRIPT_DIR/menu.sh" ]; then
-    echo -e "${RED}下载主脚本失败,请检查网络连接。${NC}"
+    echo -e "${RED}Failed to download the main script, please check the network connection.${NC}"
     exit 1
 fi
 
