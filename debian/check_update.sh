4fd65a24a0508926a53d292ab9a06ae181e87d52
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-04-12 10:24:01 +0700

    Update check_update.sh

--- debian/check_update.sh
+++ debian/check_update.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# 定义颜色
+# Defining Colors
 CYAN='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo "正在检测sing-box最新版本..."
-# 更新包信息
+echo "Detecting the latest version of sing-box..."
+# Update package information
 sudo apt-get update -qq > /dev/null 2>&1
 
-# 检查sing-box版本
+# Check sing-box version
 if command -v sing-box &> /dev/null; then
     current_version=$(sing-box version | grep 'sing-box version' | awk '{print $3}')
-    echo -e "${CYAN}当前安装的sing-box版本为:${NC} $current_version"
+    echo -e "${CYAN}The currently installed sing-box version is:${NC} $current_version"
     
-    # 获取最新稳定版本和测试版本信息
+    # Get the latest stable and test version information
     stable_version=$(apt-cache policy sing-box | grep Candidate | awk '{print $2}')
     beta_version=$(apt-cache policy sing-box-beta | grep Candidate | awk '{print $2}')
     
-    echo -e "${CYAN}稳定版最新版本：${NC} $stable_version"
-    echo -e "${CYAN}测试版最新版本：${NC} $beta_version"
+    echo -e "${CYAN}Latest stable version：${NC} $stable_version"
+    echo -e "${CYAN}Latest beta version：${NC} $beta_version"
     
-    # 提供切换版本的选项
+    # Provides the option to switch versions
     while true; do
-        read -rp "是否切换版本(1: 稳定版, 2: 测试版） (当前版本: $current_version, 回车取消操作): " switch_choice
+        read -rp "Whether to switch versions (1: stable version, 2: beta version) (current version: $current_version, press Enter to cancel the operation): " switch_choice
         case $switch_choice in
             1)
-                echo "选择了切换到稳定版"
+                echo "Selected to switch to stable version"
                 sudo apt-get install sing-box -y
                 break
                 ;;
             2)
-                echo "选择了切换到测试版"
+                echo "Selected Switch to beta"
                 sudo apt-get install sing-box-beta -y
                 break
                 ;;
