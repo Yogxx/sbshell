5cf2568cb843f9aee4d8e05da622ca72f98df513
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-04-12 10:35:34 +0700

    Update stop_singbox.sh

--- debian/stop_singbox.sh
+++ debian/stop_singbox.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# 定义颜色
+# Defining Colors
 CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NC='\033[0m' # 无颜色
+NC='\033[0m' # No Color
 
-# 脚本下载目录
+# Script download directory
 SCRIPT_DIR="/etc/sing-box/scripts"
 
-# 停止 sing-box 服务
+# Stop sing-box service
 stop_singbox() {
     sudo systemctl stop sing-box
 
     if ! systemctl is-active --quiet sing-box; then
         echo -e "${GREEN}sing-box 已停止${NC}"
 
-        # 提示用户确认是否清理防火墙规则
-        read -rp "是否清理防火墙规则？(y/n): " confirm_cleanup
+        # Prompt the user to confirm whether to clear firewall rules
+        read -rp "Do you want to clear firewall rules? (y/n): " confirm_cleanup
         if [[ "$confirm_cleanup" =~ ^[Yy]$ ]]; then
-            echo -e "${CYAN}执行清理防火墙规则...${NC}"
+            echo -e "${CYAN}Execute cleanup firewall rules...${NC}"
             bash "$SCRIPT_DIR/clean_nft.sh"
-            echo -e "${GREEN}防火墙规则清理完毕${NC}"
+            echo -e "${GREEN}Firewall rules cleared${NC}"
         else
-            echo -e "${CYAN}已取消清理防火墙规则。${NC}"
+            echo -e "${CYAN}Cleanup of firewall rules has been cancelled.${NC}"
         fi
 
     else
-        echo -e "${RED}停止 sing-box 失败，请检查日志${NC}"
+        echo -e "${RED}Failed to stop sing-box, please check the log${NC}"
     fi
 }
 
-# 提示用户确认是否停止
-read -rp "是否停止 sing-box?(y/n): " confirm_stop
+# Prompt the user to confirm whether to stop
+read -rp "Do you want to stop sing-box?(y/n): " confirm_stop
 if [[ "$confirm_stop" =~ ^[Yy]$ ]]; then
     stop_singbox
 else
-    echo -e "${CYAN}已取消停止 sing-box。${NC}"
+    echo -e "${CYAN}Stop sing-box has been cancelled.${NC}"
     exit 0
 fi
