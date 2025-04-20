769d48c21f0b871645443b995768427a9de9e4bb
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-04-20 22:51:27 +0700

    Update menu.sh

--- openwrt/menu.sh
+++ openwrt/menu.sh
@@ -22,7 +22,7 @@ if ! grep -qi 'openwrt' /etc/os-release; then
 fi
 
 # The script's URL base path
-BASE_URL="https://gh-proxy.com/https://raw.githubusercontent.com/Yogxx/sbshell/refs/heads/main/openwrt"
+BASE_URL="https://github.com/Yogxx/sbshell/tree/main/openwrt"
                                
 # 脚本列表
 SCRIPTS=(
