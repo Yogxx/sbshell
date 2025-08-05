a87813af46dbb4c64f1ec812e71dfab23eec5d74
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-08-05 15:38:50 +0700

    Update start_singbox.sh

--- openwrt/start_singbox.sh
+++ openwrt/start_singbox.sh
@@ -10,21 +10,21 @@ NC='\033[0m' # 无颜色
 # 检查当前模式
 check_mode() {
     if nft list chain inet sing-box prerouting_tproxy &>/dev/null || nft list chain inet sing-box output_tproxy &>/dev/null; then
-        echo "TProxy 模式"
+        echo "TProxy Mode"
     else
-        echo "TUN 模式"
+        echo "TUN mode"
     fi
 }
 
 # 启动 sing-box 服务
 start_singbox() {
-    echo -e "${CYAN}检测是否处于非代理环境...${NC}"
+    echo -e "${CYAN}Check if you are in a non-proxy environment...${NC}"
     STATUS_CODE=$(curl -s -o /dev/null -w '%{http_code}' --max-time 5 "https://www.google.com")
 
     if [ "$STATUS_CODE" -eq 200 ]; then
-        echo -e "${RED}当前网络处于代理环境, 启动 sing-box 需要直连!${NC}"
+        echo -e "${RED}The current network is in a proxy environment, and a direct connection is required to start sing-box!${NC}"
     else
-        echo -e "${CYAN}当前网络环境非代理网络，可以启动 sing-box。${NC}"
+        echo -e "${CYAN}The current network environment is not a proxy network, and sing-box can be started.${NC}"
     fi
 
     # 启动 sing-box 服务
@@ -34,20 +34,20 @@ start_singbox() {
     
 
     if /etc/init.d/sing-box status | grep -q "running"; then
-        echo -e "${GREEN}sing-box 启动成功${NC}"
+        echo -e "${GREEN}sing-box started successfully${NC}"
 
         mode=$(check_mode)
-        echo -e "${MAGENTA}当前启动模式: ${mode}${NC}"
+        echo -e "${MAGENTA}Current startup mode: ${mode}${NC}"
     else
-        echo -e "${RED}sing-box 启动失败，请检查日志${NC}"
+        echo -e "${RED}sing-box failed to start, please check the log${NC}"
     fi
 }
 
 # 提示用户确认是否启动
-read -rp "是否启动 sing-box?(y/n): " confirm_start
+read -rp "Do you want to start sing-box? (y/n): " confirm_start
 if [[ "$confirm_start" =~ ^[Yy]$ ]]; then
     start_singbox
 else
-    echo -e "${CYAN}已取消启动 sing-box。${NC}"
+    echo -e "${CYAN}Canceled launching sing-box.${NC}"
     exit 0
 fi
