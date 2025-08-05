96dbd318eb2c8dd4dda9cf6b308c2cf6a9e12acd
Yogxx <163709469+Yogxx@users.noreply.github.com>
2025-08-05 15:31:53 +0700

    Update manage_autostart.sh

--- openwrt/manage_autostart.sh
+++ openwrt/manage_autostart.sh
@@ -5,20 +5,20 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # 无颜色
 
-echo -e "${GREEN}设置开机自启动...${NC}"
-echo "请选择操作(1: 启用自启动, 2: 禁用自启动）"
+echo -e "${GREEN}Set auto-start at boot...${NC}"
+echo "Please select an action (1: Enable auto-start, 2: Disable auto-start)"
 read -rp "(1/2): " autostart_choice
 
 apply_firewall() {
     MODE=$(grep -oP '(?<=^MODE=).*' /etc/sing-box/mode.conf)
     if [ "$MODE" = "TProxy" ]; then
-        echo "应用 TProxy 模式下的防火墙规则..."
+        echo "Applying firewall rules in TProxy mode..."
         bash /etc/sing-box/scripts/configure_tproxy.sh
     elif [ "$MODE" = "TUN" ]; then
-        echo "应用 TUN 模式下的防火墙规则..."
+        echo "Applying firewall rules in TUN mode..."
         bash /etc/sing-box/scripts/configure_tun.sh
     else
-        echo "无效的模式，跳过防火墙规则应用。"
+        echo "Invalid mode, skips applying firewall rules."
         exit 1
     fi
 }
@@ -27,11 +27,11 @@ case $autostart_choice in
     1)
         # 检查自启动是否已经开启
         if [ -f /etc/rc.d/S99sing-box ]; then
-            echo -e "${GREEN}自启动已经开启，无需操作。${NC}"
+            echo -e "${GREEN}Automatic startup is enabled and no operation is required.${NC}"
             exit 0  # 返回主菜单
         fi
 
-        echo -e "${GREEN}启用自启动...${NC}"
+        echo -e "${GREEN}Enable autostart...${NC}"
 
         # 启用并启动服务
         /etc/init.d/sing-box enable
@@ -39,32 +39,32 @@ case $autostart_choice in
         cmd_status=$?
 
         if [ "$cmd_status" -eq 0 ]; then
-            echo -e "${GREEN}自启动已成功启用。${NC}"
+            echo -e "${GREEN}Autostart has been successfully enabled.${NC}"
         else
-            echo -e "${RED}启用自启动失败。${NC}"
+            echo -e "${RED}Failed to enable autostart.${NC}"
         fi
         ;;
     2)
         # 检查自启动是否已经禁用
         if [ ! -f /etc/rc.d/S99sing-box ]; then
-            echo -e "${GREEN}自启动已经禁用，无需操作。${NC}"
+            echo -e "${GREEN}Auto-start is disabled, no action is required.${NC}"
             exit 0  # 返回主菜单
         fi
 
-        echo -e "${RED}禁用自启动...${NC}"
+        echo -e "${RED}Disable autostart...${NC}"
         
         # 禁用并停止服务
         /etc/init.d/sing-box disable
         cmd_status=$?
 
         if [ "$cmd_status" -eq 0 ]; then
-            echo -e "${GREEN}自启动已成功禁用。${NC}"
+            echo -e "${GREEN}Autostart successfully disabled.${NC}"
         else
-            echo -e "${RED}禁用自启动失败。${NC}"
+            echo -e "${RED}Disabling autostart failed.${NC}"
         fi
         ;;
     *)
-        echo -e "${RED}无效的选择${NC}"
+        echo -e "${RED}Invalid selection${NC}"
         ;;
 esac
 
